c97b4e9d108fc551cbdde98d20bae7ebae6473f6
Matthew Young <y.matt12@gmail.com>
2022-10-06 16:56:36 -0700

    Added queries/accounts.py and routers/accounts.py

--- accounts_service/authenticator.py
+++ accounts_service/authenticator.py
@@ -2,14 +2,14 @@
 import os
 from fastapi import Depends
 from jwtdown_fastapi.authentication import Authenticator
-from queries.accounts import AccountRepo, AccountOut, Account
+from queries.accounts import AccountQueries, AccountOut, AccountIn
 
 
 class MyAuthenticator(Authenticator):
     async def get_account_data(
         self,
         username: str,
-        accounts: AccountRepo,
+        accounts: AccountQueries,
     ):
         # Use your repo to get the account based on the
         # username (which could be an email)
@@ -17,7 +17,7 @@ class MyAuthenticator(Authenticator):
 
     def get_account_getter(
         self,
-        accounts: AccountRepo = Depends(),
+        accounts: AccountQueries = Depends(),
     ):
         # Return the accounts. That's it.
         return accounts
--- accounts_service/main.py
+++ accounts_service/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
-
+from authenticator import authenticator
+from fastapi import APIRouter
 
 app = FastAPI()
+app.include_router(authenticator.router)
