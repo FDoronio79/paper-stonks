cb4755228ae5078b3670d306764685e855757b06
Filamer Doronio <filamerdoronio@gmail.com>
2022-10-11 12:58:42 -0700

    creating nav to Login and Signup

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -5,44 +5,47 @@ import './App.css';
 import MainPage from './MainPage';
 import Login from './components/Login';
 import SignupForm from './components/Signup';
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Nav from './Nav'
 
 
 function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);  
+  // const [launch_info, setLaunchInfo] = useState([]);
+  // const [error, setError] = useState(null);  
 
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
-      console.log('fastapi url: ', url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
+  // useEffect(() => {
+  //   async function getData() {
+  //     let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
+  //     console.log('fastapi url: ', url);
+  //     let response = await fetch(url);
+  //     console.log("------- hello? -------");
+  //     let data = await response.json();
 
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, [])
+  //     if (response.ok) {
+  //       console.log("got launch data!");
+  //       setLaunchInfo(data.launch_details);
+  //     } else {
+  //       console.log("drat! something happened");
+  //       setError(data.message);
+  //     }
+  //   }
+  //   getData();
+  // }, [])
 
 
   return (
-    <>
-    {/* <Searchbar /> */}
-    <div>
+    <BrowserRouter>
+      <Nav />
+        <div className="container">
+          <Routes>
       {/* <ErrorNotification error={error} />
       <Construct info={launch_info} /> */}
       {/* <MainPage /> */}
-      <Login />
-      {/* <SignupForm /> */}
-    </div>
-    </>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignupForm />} />
+          </Routes>
+        </div>
+    </BrowserRouter>
   );
 }
 
