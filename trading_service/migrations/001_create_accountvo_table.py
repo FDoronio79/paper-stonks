9580602be017ccd26f41fc98f43a0ef06cba0fc0
Matthew Young <y.matt12@gmail.com>
2022-10-11 14:22:10 -0700

    changed create table migrations into a singular file

--- trading_service/migrations/001_create_accountvo_table.py
+++ trading_service/migrations/001_create_accountvo_table.py
@@ -4,12 +4,51 @@ steps = [
         """
         CREATE TABLE accounts(
             id SERIAL NOT NULL,
-            username VARCHAR(100) PRIMARY KEY NOT NULL,
+            username VARCHAR(100) PRIMARY KEY NOT NULL
         );
         """,
         # "Down" SQL statement
         """
         DROP TABLE accounts;
         """
+    ],
+
+    [
+        # "Up" SQL statement; Create table
+        """
+        CREATE TABLE transactions (
+            id SERIAL PRIMARY KEY NOT NULL,
+            username VARCHAR(100) REFERENCES accounts(username) NOT NULL,
+            symbol VARCHAR(100) NOT NULL,
+            type_of VARCHAR(100) NOT NULL,
+            time_of_purchase TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            quantity NUMERIC NOT NULL,
+            price NUMERIC NOT NULL
+        );
+        """,
+        # "Down" SQL statement; Drop table
+        """
+        DROP TABLE transactions;
+        """
+    ],
+    [
+        # "Up" SQL statement; Create table
+        """
+        CREATE TABLE positions (
+            symbol VARCHAR(100) PRIMARY KEY NOT NULL,
+            id SERIAL UNIQUE NOT NULL,
+            username VARCHAR(100) REFERENCES accounts(username) NOT NULL,
+            name VARCHAR(100) NOT NULL,
+            quantity NUMERIC NOT NULL,
+            type_of VARCHAR(100) NOT NULL
+        );
+        """,
+        # "Down" SQL statement; Drop table
+        """
+        DROP TABLE positions;
+        """
     ]
 ]
+
+# python -m migrations up (will build the table)
+# python -m migrations down (will remove table)
