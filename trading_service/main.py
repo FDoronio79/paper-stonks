3e4199e52f5eab3cf3e83661365a98b06361a933
Leo <leo12360@gmail.com>
2022-10-11 15:23:32 -0700

    accountvo endpoints

--- trading_service/main.py
+++ trading_service/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from routers import transactions
+from routers import transactions, accountsvo
 
 app = FastAPI()
 app.include_router(transactions.router)
+app.include_router(accountsvo.router)
