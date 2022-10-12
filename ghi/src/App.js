c16e33605d23d5e5ec65ee755dea60ef558942a3
Leo <leo12360@gmail.com>
2022-10-12 13:31:26 -0700

    login redirect works

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,54 +1,54 @@
-import { useEffect, useState } from 'react';
-import Construct from './Construct.js'
-import ErrorNotification from './ErrorNotification';
-import './App.css';
-import MainPage from './MainPage';
-import Login from './components/Login';
-import SignupForm from './components/Signup';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Nav from './Nav'
-import Header from "./components/Header";
-import { UserContext } from "./context/UserContext";
-
+// import { useEffect, useState } from 'react';
+// import Construct from './Construct.js'
+// import ErrorNotification from './ErrorNotification';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import MainPage from "./MainPage";
+import SignupForm from "./components/Signup";
+import Nav from "./Nav";
+import LoginForm from "./components/Login";
+// import Header from "./components/Header";
+// import { UserContext } from "./context/UserContext";
 
 function App() {
-  // const [launch_info, setLaunchInfo] = useState([]);
-  // const [error, setError] = useState(null);  
+    // const [launch_info, setLaunchInfo] = useState([]);
+    // const [error, setError] = useState(null);
 
-  // useEffect(() => {
-  //   async function getData() {
-  //     let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
-  //     console.log('fastapi url: ', url);
-  //     let response = await fetch(url);
-  //     console.log("------- hello? -------");
-  //     let data = await response.json();
+    // useEffect(() => {
+    //   async function getData() {
+    //     let url = `${process.env.REACT_APP_API_HOST}/api/launch-details`;
+    //     console.log('fastapi url: ', url);
+    //     let response = await fetch(url);
+    //     console.log("------- hello? -------");
+    //     let data = await response.json();
 
-  //     if (response.ok) {
-  //       console.log("got launch data!");
-  //       setLaunchInfo(data.launch_details);
-  //     } else {
-  //       console.log("drat! something happened");
-  //       setError(data.message);
-  //     }
-  //   }
-  //   getData();
-  // }, [])
+    //     if (response.ok) {
+    //       console.log("got launch data!");
+    //       setLaunchInfo(data.launch_details);
+    //     } else {
+    //       console.log("drat! something happened");
+    //       setError(data.message);
+    //     }
+    //   }
+    //   getData();
+    // }, [])
 
-
-  return (
-    <BrowserRouter>
-      <Nav />
-        <div className="container">
-          <Routes>
-      {/* <ErrorNotification error={error} />
+    return (
+        <BrowserRouter>
+            <Nav />
+            <div className="container">
+                <Routes>
+                    {/* <ErrorNotification error={error} />
       <Construct info={launch_info} /> */}
-      {/* <MainPage /> */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignupForm />} />
-          </Routes>
-        </div>
-    </BrowserRouter>
-  );
+                    {/* <MainPage /> */}
+                    {/* <Route path="/" element={<MainPage />} /> */}
+                    <Route path="/login" element={<LoginForm />} />
+                    <Route path="/signup" element={<SignupForm />} />
+                    {/* <Route path="/" element={<MainPage />} /> */}
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
