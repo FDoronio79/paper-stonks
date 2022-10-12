2bb5a03e267e0f9b0c347a62953e13d9a1ab744a
Tiffany <tiffany.ameral@gmail.com>
2022-10-11 17:16:28 -0700

    Updates to positions and transactions

--- trading_service/queries/positions.py
+++ trading_service/queries/positions.py
@@ -5,7 +5,6 @@ from queries.pool import pool
 from typing import List, Union, Optional
 
 
-
 class Error(BaseModel):
     message: str
 
@@ -27,31 +26,27 @@ class PositionsOut(BaseModel):
     type_of: str
 
 
-
-
-
-
-
 class PositionRepository:
-    def get_one(self, postion_id: int) -> Optional[PositionsOut]:
+    def get_one(self, position_id: int) -> Optional[PositionsOut]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
                     result = db.execute(
                         """
-                        SELECT id, symbol, name, quantity, type_of
+                        SELECT id, username, symbol, name, quantity, type_of
                         FROM positions
                         WHERE id = %s;
                         """,
-                        [postion_id]
+                        [position_id]
                     )
                     record = result.fetchone()
                     position = PositionsOut(
                         id=record[0],
-                        symbol=record[1],
-                        name=record[2],
-                        quantity=record[3],
-                        type_of=record[4],
+                        username=record[1],
+                        symbol=record[2],
+                        name=record[3],
+                        quantity=record[4],
+                        type_of=record[5],
                     )
                 return position
         except Exception as e:
@@ -60,11 +55,11 @@ class PositionRepository:
 
     def create(self, position: PositionsIn) -> PositionsOut:
         try:
-            #connect the database
-            with pool.connection() as conn:    #will create connection
-                #get a cursor (something to run SQL with)
+            # connect the database
+            with pool.connection() as conn:  # will create connection
+                # get a cursor (something to run SQL with)
                 with conn.cursor() as db:
-                    #Run our INSERT statement
+                    # Run our INSERT statement
                     result = db.execute(
                         """
                         INSERT INTO positions
@@ -82,10 +77,9 @@ class PositionRepository:
                         ]
                     )
                     id = result.fetchone()[0]
-                    #Return new data
+                    # Return new data
                     old_data = position.dict()
                     return PositionsOut(id=id, **old_data)
         except Exception as e:
             print(e)
             return {"message": "Could not create position"}
-
