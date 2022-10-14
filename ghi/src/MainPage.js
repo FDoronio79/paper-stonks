6a6857d0ed16f220434a73f09db5d5775e29a117
Tiffany <tiffany.ameral@gmail.com>
2022-10-14 12:36:08 -0700

    Curently testing GUI stuff and adding components

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -1,11 +1,17 @@
 function MainPage() {
     return (
-        <div className="px-4 py-5 my-5 text-center">
-            <h1 className="display-5 fw-bold">Paper STONKS!</h1>
-            <div className="col-lg-6 mx-auto">
+        <div className="px-4 py-1 my-2 text-center">
+            <h1 className="display-4 fw-bold" style={{
+                color: "#198754"
+            }}>Paper STONKS!</h1>
+            {/* <div className="col-lg-6 mx-auto">
                 <p className="lead mb-4">
                     MAIN PAGE
                 </p>
+            </div> */}
+
+            <div>
+                
             </div>
         </div>
     );
