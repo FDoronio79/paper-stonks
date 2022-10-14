e18874df2c640416198953404642adce39c916a5
Leo <leo12360@gmail.com>
2022-10-14 13:58:50 -0700

    login, logout frontend working with update buying power

--- trading_service/migrations/001_create_all_tables.py
+++ trading_service/migrations/001_create_all_tables.py
@@ -14,20 +14,6 @@ steps = [
     ],
 
     [
-        # "Up" SQL statement
-        """
-        CREATE TABLE accounts(
-            id SERIAL NOT NULL,
-            username VARCHAR(100) PRIMARY KEY NOT NULL
-        );
-        """,
-        # "Down" SQL statement
-        """
-        DROP TABLE accounts;
-        """
-    ],
-
-    [
         # "Up" SQL statement; Create table
         """
         CREATE TABLE transactions (
