bc2f62c5a702776054aeb5b65792c15e6546d75d
Matthew Young <y.matt12@gmail.com>
2022-10-19 23:41:02 -0700

    protected all endpoints to require authentication

--- trading_service/routers/transactions.py
+++ trading_service/routers/transactions.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, Depends, Response
 from typing import Union, List, Optional
 from queries.transactions import TransactionIn, TransactionRepository, TransactionOut, Error
+from authenticator import authenticator
 router = APIRouter()
 
 
 @router.post("/transactions", response_model=Union[TransactionOut, Error])
 def create_transaction(transaction: TransactionIn,
                        response: Response,
+                       account_data: dict = Depends(
+                           authenticator.get_current_account_data),
                        repo: TransactionRepository = Depends()
                        ):
 
@@ -15,15 +18,18 @@ def create_transaction(transaction: TransactionIn,
 
 @router.get("/transactions", response_model=Union[Error, List[TransactionOut]])
 def get_all(
-    repo: TransactionRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    repo: TransactionRepository = Depends()
 ):
     return repo.get_all()
 
 
 @router.get("/transactions/{transaction_id}", response_model=Optional[TransactionOut])
 def get_one_transaction(
+
     transaction_id: int,
-    repo: TransactionRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    repo: TransactionRepository = Depends()
 ) -> TransactionOut:
     return repo.get_one(transaction_id)
 
