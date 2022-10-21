ac459c915ab2fd8155ce9cfbc992067587043809
Matthew Young <y.matt12@gmail.com>
2022-10-21 16:13:20 -0700

    removed get_one transaction endpoint (not used)

--- trading_service/queries/transactions.py
+++ trading_service/queries/transactions.py
@@ -29,37 +29,7 @@ class TransactionOut(BaseModel):
 
 
 class TransactionRepository:
-    def get_one(self, transaction_id: int) -> Optional[TransactionOut]:
-        try:
-            # connect the database
-            with pool.connection() as conn:  # will create connection
-                # get a cursor (something to run SQL with)
-                with conn.cursor() as db:
-                    # Run our SELECT statement
-                    result = db.execute(
-                        """
-                        SELECT id, username, symbol, type_of, time_of_purchase, quantity, price
-                        FROM transactions
-                        WHERE id = %s ;
-                        """,
-                        [transaction_id]
-                    )
-                    record = result.fetchone()
-                    transaction = TransactionOut(
-                        id=record[0],
-                        username=record[1],
-                        symbol=record[2],
-                        type_of=record[3],
-                        time_of_purchase=record[4],
-                        quantity=record[5],
-                        price=record[6],
-                    )
-                    # return self.record_to_transaction_out(record)
-                    return transaction
-        except Exception as e:
-            print(e)
-            return {"message": "Could no get that transaction"}
-
+    
     def get_all(self, username) -> Union[Error, List[TransactionOut]]:
         try:
             # connect the database
