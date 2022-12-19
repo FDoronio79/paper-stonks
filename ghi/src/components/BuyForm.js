1eee00cf3901992c5b1a1de8a7a0113264b62c08
Matthew Young <y.matt12@gmail.com>
2022-12-19 03:41:57 -0800

    linting

--- ghi/src/components/BuyForm.js
+++ ghi/src/components/BuyForm.js
@@ -1,5 +1,4 @@
 import { useState, useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 import Toast from "react-bootstrap/Toast";
 import ToastContainer from "react-bootstrap/ToastContainer";
