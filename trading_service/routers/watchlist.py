acf8341bef8517410821d96e24a2d7ca75573516
Matthew Young <y.matt12@gmail.com>
2023-01-24 13:14:49 -0800

    linting

--- trading_service/routers/watchlist.py
+++ trading_service/routers/watchlist.py
@@ -20,7 +20,8 @@ def add_to_watchlist(
 ):
     print(account_data["username"])
     watchlist_response = repo.add(
-        username=account_data["username"], watchlist=watchlist)
+        username=account_data["username"], watchlist=watchlist
+    )
     if isinstance(watchlist_response, Error):
         response.status_code = 400
     return watchlist_response
@@ -40,7 +41,9 @@ def delete_watchlist_symbol(
     account_data: dict = Depends(authenticator.get_current_account_data),
     repo: WatchlistRepository = Depends(),
 ) -> bool:
-    return repo.delete(username=account_data["username"], watchlist_symbol=watchlist_symbol)
+    return repo.delete(
+        username=account_data["username"], watchlist_symbol=watchlist_symbol
+    )
 
 
 @router.get("/watchlist/{watchlist_symbol}", response_model=bool)
@@ -49,4 +52,6 @@ def check_watchlist_for_symbol(
     account_data: dict = Depends(authenticator.get_current_account_data),
     repo: WatchlistRepository = Depends(),
 ) -> bool:
-    return repo.check_watchlist(username=account_data["username"], watchlist_symbol=watchlist_symbol)
+    return repo.check_watchlist(
+        username=account_data["username"], watchlist_symbol=watchlist_symbol
+    )
